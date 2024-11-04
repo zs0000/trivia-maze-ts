f9c4dd681d5fa6ffa5f217ab5c0a72225ae81278
zs0000 <84543723+zs0000@users.noreply.github.com>
2024-11-03 16:16:47 -0800

    d

--- vite-project/src/components/MazeComponent/MazeComponent.tsx
+++ vite-project/src/components/MazeComponent/MazeComponent.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
 import RoomCell from '../RoomCell/RoomCell';
 
 
 export default function MazeComponent({ maze }: { maze: number[][] }) {
-    let size = maze.length;
     let rowCount = 0;
     return (
         <div className={`w-full h-full flex flex-col`}>
             {maze.map((row, rowIndex) => (
                 <div className="flex flex-row w-full h-full justify-center" key={rowCount++}>
-                    {row.map((cell, cellIndex) => (
+                    {row.map((_, cellIndex) => (
                         <RoomCell key={`${rowIndex}-${cellIndex}`}  value={maze[rowIndex][cellIndex]} />
                     ))}
                 </div>
